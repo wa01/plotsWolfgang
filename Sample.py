3acd91c5e6cee8ae183ec5e8eae1e647fcee1142
Wolfgang Adam <wolfgang.adam@cern.ch>
2015-11-18 12:47:32 +0100

    prepare for subsamples

--- Sample.py
+++ Sample.py
@@ -2,6 +2,60 @@ import ROOT
 import os
 from MCReweight import *
 
+class Subsample:
+
+    def __init__(self,name,base,namelist=None,type="B", \
+                     downscale=1,filter=None,extension=False,baseweights=1.,mcReweight=None):
+        self.name = name
+        if namelist==None or namelist==[ ]:
+            self.names = [ name ]
+            if not type.lower()=="d":
+                nproc = self.getCmgInputStat(base,name)
+                self.baseweights = [ float(baseweights)/nproc ]
+            else:
+                self.baseweights = [ 1. ]
+        else:
+            self.names = namelist
+            self.baseweights = [ ]
+            bwList = hasattr(baseweights,"__iter__")
+            if bwList:
+                assert len(namelist)==len(baseweights)
+            for i,n in enumerate(namelist):
+                if not type.lower()=="d":
+                    nproc = self.getCmgInputStat(base,namelist[i])
+                    if bwList:
+                        self.baseweights.append(float(baseweights[i])/nproc)
+                    else:
+                        self.baseweights.append(float(baseweights)/nproc)
+                else:
+                    self.baseweights.append(1.)
+        self.base = base
+        self.type = type
+        self.downscale = downscale
+        self.filter = filter
+        self.file = None
+        self.fileindex = None
+        if extension:
+            self.extweight /= len(namelist)
+        if mcReweight==None:
+            self.mcReweight = None
+        else:
+            self.mcReweight = EventReweighting(mcReweight[0],mcReweight[1])
+#        print "Created sample",name,"with",self.names
+
+    def getCmgInputStat(self,base,name):
+        nproc = None
+        for l in open(base+"/"+name+"/skimAnalyzerCount/SkimReport.txt"):
+            fields = l[:-1].split()
+            if len(fields)==5 and fields[0]=="All" and fields[1]=="Events":
+                nproc = int(fields[2])
+                break
+        assert nproc!=None
+        return nproc
+        
+    def fullname(self,name):
+        return self.base+"/"+name+"/treeProducerSusySingleLepton/tree.root"
+
 class Sample:
 
     def __init__(self,name,base,namelist=None,title=None,type="B",color=1,fill=False,line=1,hatch=None, \
@@ -69,6 +123,25 @@ class Sample:
                 self.file.Close()
             n = self.names[ind]
             self.file = ROOT.TFile(self.fullname(n))
+            self.fileindex = ind
+        result = self.file.Get("tree")
+        return result
+
+    def getnextchain(self):
+        if self.fileindex==None:
+            ind = 0
+        else:
+            ind = self.fileindex + 1
+        if ind>=len(self.names):
+            self.file = None
+            self.fileindex = None
+            return None
+        if self.fileindex==None or self.fileindex!=ind:
+            if self.file!=None:
+                self.file.Close()
+            n = self.names[ind]
+            self.file = ROOT.TFile(self.fullname(n))
+            self.fileindex = ind
         result = self.file.Get("tree")
         return result
 
